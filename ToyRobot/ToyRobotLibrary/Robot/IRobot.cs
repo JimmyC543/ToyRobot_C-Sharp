8636f11c0b185ed226e9a69187d0bed7cfde4681
James Cocks <j.cocks543@gmail.com.au>
2020-02-24 23:37:07 +1100

    Create IRobot interface out of IMobile and IRotatable

---
+++ ToyRobot/ToyRobotLibrary/Robot/IRobot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToyRobotLibrary.Utilities;
+
+namespace ToyRobotLibrary.Robot
+{
+	/// <summary>
+	/// The interface for any robots our app may deal with.
+	/// </summary>
+	public interface IRobot : IRotatable, IMobile
+	{
+		public IRobot Robot();
+		//	public IPosition GetPosition();
+		//	public IOrientation GetOrientation();
+		//	private Orientation _orientation; //TODO: Generalise this; IOrientation would be ideal to better handle more general directions. Also, make nullable!
+		public void Rotate(int rotationDirection);
+		public void Move();
+	}
+}
