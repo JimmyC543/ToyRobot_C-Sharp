60e52fc5feee52b200446b3703875df1c04cf665
James Cocks <j.cocks543@gmail.com.au>
2020-02-25 13:41:42 +1100

    Add interface for Table class(es)

---
+++ ToyRobot/ToyRobotLibrary/Table/ITable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToyRobotLibrary.Utilities;
+
+namespace ToyRobotLibrary.Table
+{
+    /// <summary>
+    /// I'm using an interface here because it's conceivable that the ToyRobot could be used on different table shapes.
+    /// For this project we're starting out with a 5x5 cartesian grid, however we may want to expand on this and handle
+    /// other dimensions of Cartesian grids, or a different shape/coordinate system entirely (e.g. a flat circular tabletop)  
+    /// </summary>
+    public interface ITable
+    {
+        public bool IsValidPosition(Position position);
+    }
+}
