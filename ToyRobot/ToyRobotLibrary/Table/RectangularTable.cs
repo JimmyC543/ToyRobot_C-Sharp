8438ed54f2408f0bd189bcf2b17d27496650c2f4
James Cocks <j.cocks543@gmail.com.au>
2020-02-25 14:09:49 +1100

    Add RectangularTable class

---
+++ ToyRobot/ToyRobotLibrary/Table/RectangularTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToyRobotLibrary.Utilities;
+
+namespace ToyRobotLibrary.Table
+{
+	/// <summary>
+	/// Although the project asks for a 5x5 dimension table, it seems worth creating a more general table which doesn't necessitate equal side lengths.
+	/// I'll probably extend from this to create a SquareTable.
+	/// </summary>
+	public class RectangularTable : ITable
+	{
+
+	}
+}
