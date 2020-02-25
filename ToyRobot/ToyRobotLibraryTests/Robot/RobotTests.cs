faef3ddde3a8bae9264e4e7b5893a05f1458329f
James Cocks <j.cocks543@gmail.com.au>
2020-02-25 22:56:33 +1100

    Add more requirements to the Place method: do nothing if passed an invalid Orientation

--- ToyRobot/ToyRobotLibraryTests/Robot/RobotTests.cs
+++ ToyRobot/ToyRobotLibraryTests/Robot/RobotTests.cs
@@ -49,7 +49,6 @@ namespace ToyRobotLibraryTests.Robot
         [Theory]
         [InlineData(1, 1, 0, 0, 0)]
         [InlineData(5, 5, 3, 2, 3)]
-        [InlineData(51, 15, 31, 12, 2)]
         public void Place_ShouldSucceed_WhenPassedValidCoordinatesAndOrientation(int numRows, int numCols, int xPlacement, int yPlacement, int orientationVal)
         {
             //Arrange
@@ -75,6 +74,29 @@ namespace ToyRobotLibraryTests.Robot
         [InlineData(5, 5, 3, 5, 3)]
         [InlineData(51, 15, 31, 12, 5)]
         public void Place_ShouldDoNothing_WhenPassedInvalidCoordinates(int numRows, int numCols, int xPlacement, int yPlacement, int orientationVal)
+        {
+            //Arrange
+            var mockTable = new Mock<RectangularTable>(numRows, numCols);
+            IRobot robot = new ToyRobotLibrary.Robot.Robot(mockTable.Object);
+            var placementPosition = new Position { x = xPlacement, y = yPlacement };
+            var positionField = robot.GetType().GetField("_position", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var orientationField = robot.GetType().GetField("_orientation", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+            //Act
+            robot.Place(placementPosition, (Orientation)orientationVal);
+
+            var positionValue = positionField.GetValue(robot);
+            var orientationValue = orientationField.GetValue(robot);
+
+            //Assert
+            Assert.Null(positionValue);
+            Assert.Null(orientationValue);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 0, 0, -1)]
+        [InlineData(5, 5, 3, 3, 4)]
+        public void Place_ShouldDoNothing_WhenPassedInvalidOrientation(int numRows, int numCols, int xPlacement, int yPlacement, int orientationVal)
         {
             //Arrange
             var mockTable = new Mock<RectangularTable>(numRows, numCols);
