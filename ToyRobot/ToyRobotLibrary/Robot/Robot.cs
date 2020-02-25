fbe4767658e094a85f7ef9d774289ef251dd49ab
James Cocks <j.cocks543@gmail.com.au>
2020-02-25 15:53:46 +1100

    Add Robot class implementing methods specified in IRobot interface

---
+++ ToyRobot/ToyRobotLibrary/Robot/Robot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToyRobotLibrary.Utilities;
+
+namespace ToyRobotLibrary.Robot
+{
+    public class Robot : IRobot
+    {
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Place(Position position, Orientation orientation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReport Report()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rotate(SpinDirection rotationDirection)
+        {
+            throw new NotImplementedException();
+        }
+
+        IRobot IRobot.Robot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
