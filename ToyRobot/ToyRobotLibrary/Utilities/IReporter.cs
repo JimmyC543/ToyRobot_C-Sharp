ce573e8f1ab8ab960e018d21443748f9bd345e37
James Cocks <j.cocks543@gmail.com.au>
2020-02-26 22:25:28 +1100

    Add IReporter interface

---
+++ ToyRobot/ToyRobotLibrary/Utilities/IReporter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyRobotLibrary.Utilities
+{
+    public interface IReporter
+    {
+        public void Report(Position position, Orientation orientation);
+    }
+}
