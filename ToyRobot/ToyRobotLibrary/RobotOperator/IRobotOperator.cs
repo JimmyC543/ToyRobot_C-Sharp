8e49a651da152265687e42f867dcb641eae34eaf
James Cocks <j.cocks543@gmail.com.au>
2020-02-26 18:21:37 +1100

    Add IRobotOperator interface

---
+++ ToyRobot/ToyRobotLibrary/RobotOperator/IRobotOperator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToyRobotLibrary.Utilities;
+
+namespace ToyRobotLibrary.RobotOperator
+{
+    public interface IRobotOperator
+    {
+        public void InterpretInstruction(Instruction instruction, object[] args);
+    }
+}
