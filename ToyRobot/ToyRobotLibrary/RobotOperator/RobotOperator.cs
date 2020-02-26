2afdb64dddc12457517d03699a395f10eac9a5e9
James Cocks <j.cocks543@gmail.com.au>
2020-02-26 18:26:38 +1100

    Add RobotOperatorTests and RobotOperator, implementing IRobotOperator

---
+++ ToyRobot/ToyRobotLibrary/RobotOperator/RobotOperator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToyRobotLibrary.Robot;
+using ToyRobotLibrary.Utilities;
+
+namespace ToyRobotLibrary.RobotOperator
+{
+    public class RobotOperator : IRobotOperator
+    {
+        private readonly IRobot _robot;
+        private readonly IReporter _reporter;
+        public RobotOperator(IRobot robot, IReporter reporter)
+        {
+            _robot = robot ?? throw new ArgumentNullException(nameof(robot), "Must provide or inject object implementing IRobot");
+			_reporter = reporter ?? throw new ArgumentNullException(nameof(reporter), "Must provide or inject object implementing IReporter");
+        }
+
+		public void InterpretInstruction(Instruction instruction, object[] args)
+		{
+            throw new NotImplementedException();
+		}
+	}
+}
---
+++ ToyRobot/ToyRobotLibraryTests/RobotOperator/RobotOperatorTests.cs
@@ -0,0 +1,65 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ToyRobotLibrary.Robot;
+using ToyRobotLibrary.RobotOperator;
+using ToyRobotLibrary.Table;
+using ToyRobotLibrary.Utilities;
+using Xunit;
+
+namespace ToyRobotLibraryTests.RobotOperator
+{
+    public class RobotOperatorTests
+    {
+
+        #region Constructor tests
+
+        [Fact]
+        public void RobotOperator_ShouldSucceed_WhenPassedValidArguments()
+        {
+            //Arrange
+            Mock<IRobot> mockRobot = new Mock<IRobot>();
+            Mock<IReporter> mockReporter = new Mock<IReporter>();
+
+            //Act
+            IRobotOperator robotOperator = new ToyRobotLibrary.RobotOperator.RobotOperator(mockRobot.Object, mockReporter.Object);
+
+            //Assert
+            Assert.NotNull(robotOperator);
+            Assert.IsAssignableFrom<IRobotOperator>(robotOperator);
+            Assert.IsType<ToyRobotLibrary.RobotOperator.RobotOperator>(robotOperator);
+        }
+
+        [Fact]
+        public void RobotOperator_ShouldThrowException_WhenPassedNullRobot()
+        {
+            //Arrange
+            IRobotOperator robotOperator;
+            Mock<IReporter> mockReporter = new Mock<IReporter>();
+            //Act
+            var ex = Record.Exception(() => robotOperator = new ToyRobotLibrary.RobotOperator.RobotOperator(null, mockReporter.Object));
+
+            //Assert
+            Assert.NotNull(ex);
+            Assert.IsType<ArgumentNullException>(ex);
+            Assert.Contains("Must provide or inject object implementing IRobot", ex.Message);
+        }
+
+        [Fact]
+        public void RobotOperator_ShouldThrowException_WhenPassedNullReporter()
+        {
+            //Arrange
+            IRobotOperator robotOperator;
+            Mock<IRobot> mockRobot = new Mock<IRobot>();
+            //Act
+            var ex = Record.Exception(() => robotOperator = new ToyRobotLibrary.RobotOperator.RobotOperator(mockRobot.Object, null));
+
+            //Assert
+            Assert.NotNull(ex);
+            Assert.IsType<ArgumentNullException>(ex);
+            Assert.Contains("Must provide or inject object implementing IReporter", ex.Message);
+        }
+        #endregion
+    }
+}
