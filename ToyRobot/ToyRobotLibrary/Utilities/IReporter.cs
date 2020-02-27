5125b158c9eb90dfe8d20a25ea09785c2a544d29
James Cocks <j.cocks543@gmail.com.au>
2020-02-27 14:28:33 +1100

    Add clarification for IReporter interface

--- ToyRobot/ToyRobotLibrary/Utilities/IReporter.cs
+++ ToyRobot/ToyRobotLibrary/Utilities/IReporter.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace ToyRobotLibrary.Utilities
 {
+    /// <summary>
+    /// Classes implementing this interface will be dealing with the output of our application,
+    /// whether it's the console/command line, an output file, or even a web api.
+    /// </summary>
     public interface IReporter
     {
         public void Report(Position position, Orientation orientation);
