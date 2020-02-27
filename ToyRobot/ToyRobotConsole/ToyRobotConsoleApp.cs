acee869102b6611f20c8600a204cb6abb23c03a6
James Cocks <j.cocks543@gmail.com.au>
2020-02-28 01:47:48 +1100

    Refactor Execute method to extract parsing of input to its own function.

--- ToyRobot/ToyRobotConsole/ToyRobotConsoleApp.cs
+++ ToyRobot/ToyRobotConsole/ToyRobotConsoleApp.cs
@@ -24,26 +24,40 @@ namespace ToyRobotConsole
 			_reader = reader ?? throw new ArgumentNullException(nameof(reader));
 		}
 
+		/// <summary>
+		/// Reads the user's input and parses it.
+		/// </summary>
+		/// <param name="instruction">Out parameter: if successful, it will be one of the Instruction enum values.</param>
+		/// <param name="args">Possible additional arguments provided by the user.</param>
+		/// <returns>True if the instruction provided by the use matches a valid instruction, and the parsing was successful. Else, false.</returns>
+		private bool TryParseInput(out Instruction instruction, out object[] args)
+		{
+			var textInput = _reader.ReadInstruction();
+			var distinctWords = textInput.Trim().Split(' ', ',');
+			var maybeInstruction = distinctWords[0];
+			args = distinctWords.Length > 1 ? distinctWords.Skip(1).ToArray() : null;
+
+			//Enum.TryParse will not only accept strings, but the underlying enum values.
+			//Since we don't want to parse e.g. "0" as "PLACE", check that the text matches one of the Instruction enum Names
+			//as well as trying to parse it.
+			return Enum.TryParse(maybeInstruction, ignoreCase: true, out instruction)
+				&& Enum.GetNames(typeof(Instruction)).Contains(maybeInstruction.ToUpper());
+		}
+
+		/// <summary>
+		/// Runs an infinite loop, listening for user input and acting accordingly (if valid).
+		/// </summary>
 		public void Execute()
 		{
 			while (true)
 			{
-				var textInput = _reader.ReadInstruction();
-				var distinctWords = textInput.Trim().Split(' ', ',');
-				var maybeInstruction = distinctWords[0];
-				IEnumerable<object> args = distinctWords.Length > 1 ? distinctWords.Skip(1) : null;
-
-				//Enum.TryParse will not only accept strings, but the underlying enum values.
-				//Since we don't want to parse e.g. "0" as "PLACE", check that the text matches one of the Instruction enum Names
-				//as well as trying to parse it.
-				bool isRecognisedInstruction = Enum.TryParse(maybeInstruction, ignoreCase: true, out Instruction instruction)
-					&& Enum.GetNames(typeof(Instruction)).Contains(maybeInstruction.ToUpper());
+				bool isRecognisedInstruction = TryParseInput(out Instruction instruction, out object[] args);
 
 				if (isRecognisedInstruction)
 				{
 					//It's not clear what should happen in the event of an invalid instruction or argument
 					//I'm going to let the app crash
-					_robotOperator.InterpretInstruction(instruction, args?.ToArray());
+					_robotOperator.InterpretInstruction(instruction, args);
 				}
 			}
 		}
